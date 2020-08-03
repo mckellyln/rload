7483cfc1b35b548f07b92dc05cb896bd76bc75f8
Mark Kelly <mark.kelly@lexisnexisrisk.com>
2020-08-03 12:47:47 -0400

    More devl

--- rload.cpp
+++ rload.cpp
@@ -55,6 +55,9 @@ using namespace std;
     1         2         3          4     5                               6                                 7                8                          9
 00042163 2020-07-31 09:58:25.248 42675 68625 "[10.173.104.100:9876{esp_172.16.70.157_160012413R144982}] COMPLETE: Risk_Indicators.InstantID esp_172.16.70.157_160012413R144982 from 172.16.70.157 complete in 156 msecs memory=167 Mb priority=-2 slavesreply=331997 resultsize=5745 continue=0 NumRowsProcessed=62873 NumIndexSeeks=1708 NumIndexScans=7005 NumIndexWildSeeks=1125 NumIndexMerges=4 NumLeafCacheHits=1023 NumNodeCacheHits=1361 NumLeafCacheAdds=146 NumNodeCacheAdds=34 NumIndexAccepted=6778 NumIndexRejected=6331 NumIndexRowsRead=13089 TimeSoapcall=8.803ms TimeSortElapsed=506402ns NumGroups=343 NumGroupMax=2624 NumAllocations=92101 NumAllocationScans=32982 fDMetaphone1={ NumStarted=243 NumStopped=243 CycleLocalExecuteCycles=366065 } fCleanLNBO={ NumStarted=1 NumStopped=1 CycleLocalExecuteCycles=2304431 }"
 
+    1         2         3          4     5      6      7        8   9
+00000001 2020-07-14 21:21:46.494 70720 70720 "Roxie starting, build = internal_6.4.28-1"
+
 */
 
 int main(int argc, char *argv[])
@@ -90,7 +93,7 @@ int main(int argc, char *argv[])
     int active = 0;
     int maxact = 0;
 
-    unsigned int prevpid;
+    unsigned int prevpid = 0;
 
     char ftime[256] = { "" };
     char mtime[256] = { "" };
@@ -109,69 +112,69 @@ int main(int argc, char *argv[])
         int rtn = sscanf(line, "%x%s%s%u%u%s%s%s", &logid, ldate, ltime, &pid, &tid, qid, qstate, misc);
         if (rtn != 8)
             continue;
-        if ( (strcmp(qstate, "QUERY:") == 0) || (strcmp(qstate, "COMPLETE:") == 0) )
+
+        char ltime2[256] = { "" };
+        strcpy(ltime2, ltime);
+
+        // 09:58:25.700
+        char *hr = strtok(ltime2,":");
+        char *min = strtok(NULL,":");
+        char *sec = strtok(NULL,":");
+
+        if ( (hr == NULL) || (min == NULL) || (sec == NULL) )
+            continue;
+
+        char date_time[512] = { "" };
+        sprintf(date_time, "%s %s:%s:%s", ldate, hr, min, sec);
+
+        time_t stime, etime;
+        if (fline)
         {
-            char ltime2[256] = { "" };
-            strcpy(ltime2, ltime);
+            fline = 0;
+            struct tm tm0, tm1;
+            memset(&tm0, 0, sizeof(struct tm));
+            memset(&tm1, 0, sizeof(struct tm));
+            char date_time0[512] = { "" };
+            char date_time1[512] = { "" };
 
-            // 09:58:25.700
-            char *hr = strtok(ltime2,":");
-            char *min = strtok(NULL,":");
-            char *sec = strtok(NULL,":");
+            sprintf(date_time0, "%s %s", ldate, start_time);
+            strptime(date_time0, "%Y-%m-%d %H:%M:%S", &tm0);
+            stime = mktime(&tm0);
 
-            if ( (hr == NULL) || (min == NULL) || (sec == NULL) )
-                continue;
+            sprintf(date_time1, "%s %s", ldate, end_time);
+            strptime(date_time1, "%Y-%m-%d %H:%M:%S", &tm1);
+            etime = mktime(&tm1);
+        }
 
-            char date_time[512] = { "" };
-            sprintf(date_time, "%s %s:%s:%s", ldate, hr, min, sec);
+        struct tm tm;
+        memset(&tm, 0, sizeof(struct tm));
+        strptime(date_time, "%Y-%m-%d %H:%M:%S", &tm);
+        time_t qtime = mktime(&tm);
 
-            time_t stime, etime;
-            if (fline)
+        if ( (qtime >= stime) && (qtime < etime) )
+        {
+            if ( (!fpid) && (strcmp(qid, "\"Roxie") == 0) && (strcmp(qstate, "starting,") == 0) && (strcmp(misc, "build") == 0) )
             {
-                fline = 0;
-                struct tm tm0, tm1;
-                memset(&tm0, 0, sizeof(struct tm));
-                memset(&tm1, 0, sizeof(struct tm));
-                char date_time0[512] = { "" };
-                char date_time1[512] = { "" };
-
-                sprintf(date_time0, "%s %s", ldate, start_time);
-                strptime(date_time0, "%Y-%m-%d %H:%M:%S", &tm0);
-                stime = mktime(&tm0);
-
-                sprintf(date_time1, "%s %s", ldate, end_time);
-                strptime(date_time1, "%Y-%m-%d %H:%M:%S", &tm1);
-                etime = mktime(&tm1);
+                printf("-- restart time: %s\n", ltime);
+                printf("Roxie pid (%u) changed (prev: %u) ...\n", pid, prevpid);
+                printf("num queries = %d\n", numq);
+                printf("active =      %d\n", active);
+                printf("max active  = %d\n", maxact);
+                printf("max active time: %s\n", mtime);
+                printf("----------------\n");
+                numq = 0;
+                active = 0;
+                maxact = 0;
+                qmap.clear();
+                fpid = 1;
             }
-
-            struct tm tm;
-            memset(&tm, 0, sizeof(struct tm));
-            strptime(date_time, "%Y-%m-%d %H:%M:%S", &tm);
-            time_t qtime = mktime(&tm);
-
-            if ( (qtime >= stime) && (qtime < etime) )
+            else if ( (strcmp(qstate, "QUERY:") == 0) || (strcmp(qstate, "BLIND:") == 0) || (strcmp(qstate, "COMPLETE:") == 0) )
             {
-
                 if (fpid)
                 {
                     fpid = 0;
                     prevpid = pid;
                     printf("start time: %s\n", ltime);
-                }
-                else if (prevpid != pid)
-                {
-                    printf("-- time: %s\n", ltime);
-                    printf("Roxie pid (%u) changed (prev: %u) ...\n", pid, prevpid);
-                    printf("num queries = %d\n", numq);
-                    printf("active =      %d\n", active);
-                    printf("max active  = %d\n", maxact);
-                    printf("max active time: %s\n", mtime);
-                    printf("----------------\n");
-                    numq = 0;
-                    active = 0;
-                    maxact = 0;
-                    qmap.clear();
-                    prevpid = pid;
                 }
 
                 char qid2[256] = { "" };
@@ -182,7 +185,7 @@ int main(int argc, char *argv[])
                 char key[300] = { "" };
                 sprintf(key, "%s:%u", qid2, tid);
 
-                if (strcmp(qstate, "QUERY:") == 0)
+                if ( (strcmp(qstate, "QUERY:") == 0) || (strcmp(qstate, "BLIND:") == 0) )
                 {
                     // add qid to list
                     // incr active query count
@@ -209,10 +212,14 @@ int main(int argc, char *argv[])
                         qmap.erase(res);
                         active--;
                     }
+                    else
+                        printf("Error, unable to locate <%s> in map\n", key);
                 }
 
             }
         }
+        else if (qtime > etime)
+            break;
     }
 
     fclose(fp);
@@ -221,7 +228,8 @@ int main(int argc, char *argv[])
     printf("num queries = %d\n", numq);
     printf("active =      %d\n", active);
     printf("max active  = %d\n", maxact);
-    printf("max active time: %s\n", mtime);
+    if (maxact > 0)
+        printf("max active time: %s\n", mtime);
 
     return 0;
 }
